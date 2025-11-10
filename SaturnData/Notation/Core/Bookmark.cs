a38c710d110e110e7a2e95d6f6a69e7431739523
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-11-10 19:25:14 +0100

    Update Bookmark.cs

--- SaturnData/Notation/Core/Bookmark.cs
+++ SaturnData/Notation/Core/Bookmark.cs
@@ -1,16 +1,10 @@
+using System;
 using SaturnData.Notation.Interfaces;
 
 namespace SaturnData.Notation.Core;
 
-public class Bookmark : ITimeable
+public class Bookmark : ITimeable, ICloneable
 {
-    public Bookmark(Bookmark cloneSource)
-    {
-        Timestamp = new(cloneSource.Timestamp.FullTick);
-        Color = cloneSource.Color;
-        Message = cloneSource.Message;
-    }
-    
     public Bookmark(Timestamp timestamp, uint color, string message)
     {
         Timestamp = timestamp;
@@ -23,4 +17,6 @@ public class Bookmark : ITimeable
     public uint Color { get; set; }
     
     public string Message { get; set; }
+
+    public object Clone() => new Bookmark(new(Timestamp), Color, Message);
 }
