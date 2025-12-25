82437bb1f000206ef5687448cdac5e6e081b2a2e
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-12-25 17:08:48 +0100

    remove kawazuconverter (moved to SaturnEdit)
    this is to resolve a dependency issue. Unity only supports .NET Standard 2.1, and kawazu converter only supports net5.0, netcoreapp3.1, netstandard 2.0.

--- SaturnData/Notation/Core/Entry.cs
+++ SaturnData/Notation/Core/Entry.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Kawazu;
 using SaturnData.Content.Music;
 using SaturnData.Notation.Serialization;
 
@@ -75,6 +71,8 @@ public class Entry
     /// It's recommended to use a universally unique identifier (UUID) for the Id, but the value doesn't have to conform to any standard.
     /// </remarks>
     public string Id = NewId;
+    
+    public static string NewId => $"SAT{Guid.NewGuid()}";
 
     /// <summary>
     /// The format version of the file that defined this entry.
@@ -614,93 +612,4 @@ public class Entry
             _ => 0.8f,
         };
     }
-
-    /// <summary>
-    /// Returns the "reading" string for a specified title.
-    /// </summary>
-    public static async Task<string> GetAutoReading(string title)
-    {
-        string result = title;
-
-        foreach (KeyValuePair<char, char> pair in FullWidthDict)
-        {
-            result = result.Replace(pair.Key, pair.Value);
-        }
-
-        KawazuConverter converter = new();
-        result = await converter.Convert(result);
-
-        result = new(result.Where(c => ValidCharacters.Contains(c)).ToArray());
-
-        return result;
-    }
-    
-    public static string NewId => $"SAT{Guid.NewGuid()}";
-    
-    private static readonly HashSet<char> ValidCharacters =
-    [
-        'あ', 'い', 'う', 'え', 'お',
-        'か', 'き', 'く', 'け', 'こ',
-        'さ', 'し', 'す', 'せ', 'そ',
-        'た', 'ち', 'つ', 'て', 'と',
-        'な', 'に', 'ぬ', 'ね', 'の',
-        'は', 'ひ', 'ふ', 'へ', 'ほ',
-        'ま', 'み', 'む', 'め', 'も',
-        'や', 'ゆ', 'よ',
-        'ら', 'り', 'る', 'れ', 'ろ',
-        'わ', 'ゐ', 'ゑ', 'を',
-        'が', 'ぎ', 'ぐ', 'げ', 'ご',
-        'ざ', 'じ', 'ず', 'ぜ', 'ぞ',
-        'だ', 'ぢ', 'づ', 'で', 'ど',
-        'ば', 'び', 'ぶ', 'べ', 'ぼ',
-        'ぱ', 'ぴ', 'ぷ', 'ぺ', 'ぽ',
-        'ん','っ',
-        
-        'ぁ','ぃ','ぅ','ぇ','ぉ',
-        'ゃ','ゅ','ょ',
-
-        '１', '２', '３', '４', '５', '６', '７', '８', '９', '０',
-        'Ａ', 'Ｂ', 'Ｃ', 'Ｄ', 'Ｅ', 'Ｆ', 'Ｇ', 'Ｈ', 'Ｉ', 'Ｊ', 'Ｋ', 'Ｌ', 'Ｍ', 'Ｎ', 'Ｏ', 'Ｐ', 'Ｑ', 'Ｒ', 'Ｓ', 'Ｔ', 'Ｕ', 'Ｖ', 'Ｗ', 'Ｘ', 'Ｙ', 'Ｚ',
-    ];
-
-    private static readonly Dictionary<char, char> FullWidthDict = new()
-    {
-        ['A'] = 'Ａ', ['a'] = 'Ａ',
-        ['B'] = 'Ｂ', ['b'] = 'Ｂ',
-        ['C'] = 'Ｃ', ['c'] = 'Ｃ',
-        ['D'] = 'Ｄ', ['d'] = 'Ｄ',
-        ['E'] = 'Ｅ', ['e'] = 'Ｅ',
-        ['F'] = 'Ｆ', ['f'] = 'Ｆ',
-        ['G'] = 'Ｇ', ['g'] = 'Ｇ',
-        ['H'] = 'Ｈ', ['h'] = 'Ｈ',
-        ['I'] = 'Ｉ', ['i'] = 'Ｉ',
-        ['J'] = 'Ｊ', ['j'] = 'Ｊ',
-        ['K'] = 'Ｋ', ['k'] = 'Ｋ',
-        ['L'] = 'Ｌ', ['l'] = 'Ｌ',
-        ['M'] = 'Ｍ', ['m'] = 'Ｍ',
-        ['N'] = 'Ｎ', ['n'] = 'Ｎ',
-        ['O'] = 'Ｏ', ['o'] = 'Ｏ',
-        ['P'] = 'Ｐ', ['p'] = 'Ｐ',
-        ['Q'] = 'Ｑ', ['q'] = 'Ｑ',
-        ['R'] = 'Ｒ', ['r'] = 'Ｒ',
-        ['S'] = 'Ｓ', ['s'] = 'Ｓ',
-        ['T'] = 'Ｔ', ['t'] = 'Ｔ',
-        ['U'] = 'Ｕ', ['u'] = 'Ｕ',
-        ['V'] = 'Ｖ', ['v'] = 'Ｖ',
-        ['W'] = 'Ｗ', ['w'] = 'Ｗ',
-        ['X'] = 'Ｘ', ['x'] = 'Ｘ',
-        ['Y'] = 'Ｙ', ['y'] = 'Ｙ',
-        ['Z'] = 'Ｚ', ['z'] = 'Ｚ',
-        
-        ['1'] = '１', 
-        ['2'] = '２', 
-        ['3'] = '３', 
-        ['4'] = '４', 
-        ['5'] = '５', 
-        ['6'] = '６', 
-        ['7'] = '７', 
-        ['8'] = '８', 
-        ['9'] = '９', 
-        ['0'] = '０',
-    };
 }
